0db3f0208b71f2bc1f7220b12c2cf4be6a670704
kav7ansh-gupta <kavyanshgupta119@gmail.com>
2026-03-25 09:27:38 +0530

    first commit

--- index.js
+++ index.js
@@ -49,5 +49,5 @@ ${code}
 });
 const PORT = process.env.PORT || 5000;
 app.listen(5000, () => {
-  onsole.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
